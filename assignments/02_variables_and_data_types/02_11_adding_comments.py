cb9bdb073485cd89c46a9f4770466904d41556de
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-09 22:41:20 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,8 @@
+# My favorite number is 21 because its the number I wear in baseball
+favorite_number = 'my favorite number is 21'
+print(favorite_number)
+# Nick Graziano 9/9/25 this program prints all these equations that come out to 8
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
