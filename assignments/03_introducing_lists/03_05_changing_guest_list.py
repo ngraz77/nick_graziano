d4c73f140ce89121c810827723112a94a4013078
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-11 12:34:31 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_05_changing_guest_list.py
@@ -0,0 +1,7 @@
+guests = ['Lebron James', 'Cristiano Ronaldo', 'Justin Fields', 'Garret Wilson']
+
+message = ["I love watching you play basketball, please come to my dinner", "You're the goat, please come to my dinner", "couldn't make it. Im a huge jets fan, please come to my dinner"]
+
+print (message[0], guests[0])
+print (message[1], guests[1])
+print (guests[2], message[2],guests[3])
