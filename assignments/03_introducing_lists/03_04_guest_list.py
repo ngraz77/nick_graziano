c426140fb0c8fe05ed608ca4401ab84c6af41802
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-11 12:12:44 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_04_guest_list.py
@@ -0,0 +1,7 @@
+guests = ['Lebron James', 'Cristiano Ronaldo', 'Justin Fields']
+
+message = ["I love watching you play basketball, please come to my dinner", "You're the goat, please come to my dinner", "Im a huge jets fan, please come to my dinner"]
+
+print (message[0], guests[0])
+print (message[1], guests[1])
+print (message[2], guests[2])
