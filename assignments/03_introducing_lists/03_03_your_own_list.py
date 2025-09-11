038c546dd13fd324dc7ba6b5e203e8f6001b9ebf
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-11 11:57:46 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,6 @@
+transport = ['bikes', 'car', 'train', 'plane']
+
+statement = ["are good to ride around the block", "I love to drive my new"]
+
+print (transport[0], statement[0])
+print (statement[1], transport[1])
