a7979d40df3701db7eed44bd8a8e16d63fc0919c
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-16 11:59:35 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_09_dinner_guests.py
@@ -0,0 +1,17 @@
+guests = ['Lebron James', 'Cristiano Ronaldo', 'Justin Fields', 'Garret Wilson', 'Francisco Lindor', 'Sauce Gardener', 'Jalen Brunson']
+
+
+message2 = ["I can only invite 2 people for dinner"]
+
+message = ["you're still invited", "sorry you cant come anymore,"]
+
+print (message2)
+print (message[0], guests[0])
+print (message[0], guests[1])
+print (message[1], guests[2])
+print (message[1], guests[3])
+print (message[1], guests[4])
+print (message[1], guests[5])
+print (message[1], guests[6])
+
+print ("I'm inviting two people to dinner")
