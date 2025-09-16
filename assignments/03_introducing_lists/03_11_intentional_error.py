c0d02c82aed34d0d8f8013a0b51e138d4cc03a0f
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-16 12:25:14 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_11_intentional_error.py
@@ -0,0 +1,10 @@
+Cities =['NYC', 'LA', 'Chicago', 'Boston']
+
+Message = ["One day I would love to go to"]
+
+print (Cities)
+print (sorted(Cities))
+
+print (Message[0], Cities[1])
+print (Cities[5])
+
