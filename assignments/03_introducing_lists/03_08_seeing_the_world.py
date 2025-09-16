a98feb124dfe53156ca886dfed10ac38d87b7f57
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-16 11:57:45 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_08_seeing_the_world.py
@@ -0,0 +1,8 @@
+Locations = ['Italy', 'California', 'Japan', 'Hawaii']
+
+print ("Here is the original list")
+print (Locations)
+print ("Here is the sorted list")
+print (sorted(Locations))
+
+print (reversed(Locations))
