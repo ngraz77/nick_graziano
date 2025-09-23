e4c3713f4fdf6c51ef6e6cae1a199d452831d281
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-23 12:33:39 -0400

    Add files via upload

---
+++ assignments/04_working_with_lists/04_10_slice.py
@@ -0,0 +1,9 @@
+Animals =[ '''Penguin''', '''Hawk''', '''Eagle''']
+print("The first 2 animals in this list are")
+print(Animals[0:2])
+
+print("The middle animal in this list is")
+print(Animals[1:2])
+
+print("The last 2 animals in this list are")
+print(Animals[1:3])
