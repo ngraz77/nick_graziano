fe1d6aa2a7375ca7b7816bcabf47d137af319db9
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-23 11:33:16 -0400

    Add files via upload

---
+++ assignments/04_working_with_lists/04_02_animals.py
@@ -0,0 +1,5 @@
+Animals =[ '''Penguin''', '''Hawk''', '''Eagle''']
+for bird in Animals:
+    print(f"{bird.title()} is a cool bird!")
+    print(f"{bird.title()} would make such a cool pet")
+    print("All of these animals are birds!")
