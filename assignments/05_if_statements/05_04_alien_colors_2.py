9ca94c56e8fae8a6549493d468069960f65f8998
ngraz77 <nicholas.graziano001@albright.edu>
2025-09-30 12:25:14 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_04_alien_colors_2.py
@@ -0,0 +1,7 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print ('the player has earned 5 points')
+else:
+    alien_color = 'red' or 'yellow'
+    print ('player has earned 10 points')
+    
