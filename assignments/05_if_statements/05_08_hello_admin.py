346223bdfdaa7f55b1d6d24a2bd49a37db9d7562
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-02 13:11:06 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_08_hello_admin.py
@@ -0,0 +1,13 @@
+users = ['admin', "nick", "power man", "penguin", "PBX"]
+
+for i in users:
+    if i == 'admin':
+        print ("You caanot touch admnin's power")
+    else:
+        print (f"{i} has much power.")
+
+    if i == 'nick':
+        print("Hello Nick, thank you for logging on again!")
+
+    if i == 'power man':
+        print ("Hello Power Man, thank you for logging on again!")
