6429526f200cd31e43650e022709227b4229dcdf
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-02 11:28:41 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_06_stages_of_life.py
@@ -0,0 +1,14 @@
+age = 2
+
+if age < 2:
+    print ("Such a baby")
+elif age < 4:
+    print ("Just a toddler")
+elif age < 13:
+    print ("What a kid")
+elif age < 20:
+    print ("Such a teenager")
+elif age < 65:
+    print ("What an adult")
+elif age > 65:
+    print ("what an elder")
