31c632e3b020c0994f09bc38c89dda862b3249ad
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-02 11:40:24 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_07_favorite_fruit.py
@@ -0,0 +1,7 @@
+favorite_food = ("Bananas", "Pinapples", "Blueberries")
+if favorite_food == 'Bananas':
+    print ('they are so tasty')
+elif favorite_food == 'Pinapples':
+    print ('they taste great')
+elif favorite_food == 'Blueberries':
+    print ('I love them')
