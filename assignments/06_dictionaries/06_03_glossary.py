cc8e67bff1870c4ee0777edfec0c0356965b3cf4
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-16 11:42:13 -0400

    Add files via upload

---
+++ assignments/06_dictionaries/06_03_glossary.py
@@ -0,0 +1,15 @@
+Python_words = {'list': 'stores a collection of items', 'dictionary': 'stores data in key value pairs', 'variable': 'symbolic name for a value', 'string': 'sequence of characters enclosed in quotes', 'integer': 'a whole number, positive or negative'}
+print ('list')
+print (Python_words['list'])
+
+print ('dictionary')
+print (Python_words['dictionary'])
+
+print ('variable')
+print (Python_words['variable'])
+
+print ('string')
+print (Python_words['string'])
+
+print ('integer')
+print(Python_words['integer'])
