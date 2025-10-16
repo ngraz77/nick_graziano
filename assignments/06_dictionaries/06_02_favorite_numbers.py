ee054276ff158ef2a93d72fcc8f6a8e82a243a1e
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-16 11:25:19 -0400

    Add files via upload

---
+++ assignments/06_dictionaries/06_02_favorite_numbers.py
@@ -0,0 +1,11 @@
+Nick = {'favorite_number': '21'}
+JD = {'favorite_number': '7'}
+AJ = {'favorite_number': '35'}
+Eric = {'favorite_number': '10'}
+Matt = {'favorite_number': '23'}
+
+print (Nick['favorite_number'])
+print (JD['favorite_number'])
+print (AJ['favorite_number'])
+print (Eric['favorite_number'])
+print (Matt['favorite_number'])
