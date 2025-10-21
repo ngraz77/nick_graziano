f027b010daa4e7e1c155915614b12b5e530e5f4d
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-21 12:16:33 -0400

    Add files via upload

---
+++ assignments/07_user_input_and_while_loops/07_06_three_exits.py
@@ -0,0 +1,21 @@
+print("Welcome to the movie theater!")
+print("Enter 'quit' at any time to stop.\n")
+
+active = True
+
+while active:
+    age_input = input("Please enter your age: ")
+
+    if age_input.lower() == 'quit':
+        print("Thank you for visiting! Enjoy your movie!")
+        active = False
+    else:
+        age = int(age_input)
+
+        if age < 3:
+            print("Your ticket is free!\n")
+        elif age <= 12:
+            print("Your ticket costs $10.\n")
+        else:
+            print("Your ticket costs $15.\n")
+
