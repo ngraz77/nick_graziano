39003d3eb577423d390c2132ce62f3f410f335b3
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-21 11:49:00 -0400

    Add files via upload

---
+++ assignments/07_user_input_and_while_loops/07_03_multiples_of_ten.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+
+if number % 10 == 0:
+    print(f"The number {number} is a multiple of 10.")
+else:
+    print(f"The number {number} is not a multiple of 10.")
