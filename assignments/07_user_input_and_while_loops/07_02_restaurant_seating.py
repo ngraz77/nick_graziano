d44bed37b51da618a25831517ad3d71c80749a05
ngraz77 <nicholas.graziano001@albright.edu>
2025-10-21 11:38:55 -0400

    Add files via upload

---
+++ assignments/07_user_input_and_while_loops/07_02_restaurant_seating.py
@@ -0,0 +1,6 @@
+people = int(input("How many people are in your dinner group? "))
+
+if people > 8:
+    print("I'm sorry, you'll have to wait for a table.")
+else:
+    print("Your table is ready!")
